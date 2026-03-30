4e5ebb3db6bf2590627207fd9f7e4a93d264931e
Sinthuja thiyagarajan <sinthujathiyagarajank@gmail.com>
2026-03-30 13:52:11 +0530

    Delete MDK-ARM/7_SEGMENT directory

--- MDK-ARM/7_SEGMENT/hc595.d
+++
@@ -1,5 +0,0 @@
-7_segment/hc595.o: hc595.c \
-  ..\Drivers\CMSIS\Device\ST\STM32G0xx\Include\stm32g070xx.h \
-  ..\Drivers\CMSIS\Include\core_cm0plus.h \
-  ..\Drivers\CMSIS\Device\ST\STM32G0xx\Include\system_stm32g0xx.h \
-  seven-segment.h hc595.h
--- MDK-ARM/7_SEGMENT/seven-segment.d
+++
@@ -1,1 +0,0 @@
-7_segment/seven-segment.o: seven-segment.c seven-segment.h
